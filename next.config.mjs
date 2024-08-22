4fc23a5d03ad3fa9cc17f302e011f3d96291fa85
oscar1223 <oscararauzp@gmail.com>
2024-08-22 13:56:46 +0200

    full refactor of the project

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: function(config, options) {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
